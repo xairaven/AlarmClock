67dc589d54ecd3d8c3e0439c0281a4045d6c57c8
xairaven <alexkovalevkpi@gmail.com>
2024-03-22 02:12:44 +0200

    (Lab 3) Feat: Custom title bar User control

---
+++ Lab3/Views/Controls/CustomTitleBar.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Lab3.Views.Controls;
+
+public partial class CustomTitleBar : UserControl
+{
+    public CustomTitleBar()
+    {
+        InitializeComponent();
+    }
+    
+    private void Exit_Window(object sender, RoutedEventArgs e)
+    {
+        var window = GetWindow();
+        window.Close();
+    }
+
+    private void Resize_Window(object sender, RoutedEventArgs e)
+    {
+        var window = GetWindow();
+        window.WindowState = window.WindowState == WindowState.Maximized 
+            ? WindowState.Normal : WindowState.Maximized;
+    }
+
+    private void Minimize_Window(object sender, RoutedEventArgs e)
+    {
+        var window = GetWindow();
+        window.WindowState = WindowState.Minimized;
+    }
+
+    private Window GetWindow()
+    {
+        var window = Window.GetWindow(this);
+
+        if (window is null) ErrorShutdown();
+        
+        return window!;
+    }
+
+    private void ErrorShutdown()
+    {
+        MessageBox.Show("Error. Unable to get current window (Custom title bar)",
+            caption: "Error!",
+            button: MessageBoxButton.OK,
+            icon: MessageBoxImage.Error);
+                
+        Application.Current.Shutdown();
+    }
+}
