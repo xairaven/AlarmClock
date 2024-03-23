b6ba7b517089d335b99bb9d450d04b68d4d1990b
xairaven <alexkovalevkpi@gmail.com>
2024-03-23 22:17:10 +0200

    (Views) Feat: Added Syncfusion DatePicker and TimePicker

--- AlarmClock/Views/AddAlarmWindow.xaml.cs
+++ AlarmClock/Views/AddAlarmWindow.xaml.cs
@@ -7,5 +7,26 @@ public partial class AddAlarmWindow : Window
     public AddAlarmWindow()
     {
         InitializeComponent();
+        DatePicker.MinDate = DateTime.Now;
+        TimePicker.MinTime = DateTime.Now.TimeOfDay;
+    }
+
+    private void CancelAddAlarm(object sender, RoutedEventArgs e)
+    {
+        Close();
+    }
+
+    private void SaveAlarm(object sender, RoutedEventArgs e)
+    {
+        
+    }
+
+    private void DatePicker_OnValueChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        var date = DatePicker.Value;
+        if (date is not null && date.Value.CompareTo(DateTime.Now) == 1)
+        {
+            TimePicker.MinTime = new TimeSpan(00,00,00);
+        }
     }
 }
