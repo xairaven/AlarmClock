46be201dc671b80a314daf516110cfc06a4c4e31
xairaven <alexkovalevkpi@gmail.com>
2024-03-23 02:07:59 +0200

    (Lab 3) Feat: New Control - Alarm Element

---
+++ Lab3/Views/Controls/AlarmElement.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace Lab3.Views.Controls;
+
+public partial class AlarmElement : UserControl
+{
+    [Description("Time. Example: 00:00"), Category("Alarm")] 
+    public string Time {
+        get => (string) TimeLabel.Content;
+        set => TimeLabel.Content = value;
+    }
+    
+    [Description("Date. Example: Fri, 23 Mar"), Category("Alarm")] 
+    public string Date {
+        get => (string) DateLabel.Content;
+        set => DateLabel.Content = value;
+    }
+
+    [Description("Status of alarm, On/Off"), Category("Alarm")]
+    public bool IsAlarmEnabled = true;
+    
+    public AlarmElement()
+    {
+        InitializeComponent();
+        GetImageCorrespondingStatus();
+    }
+
+    private void GetImageCorrespondingStatus()
+    {
+        var offImage = new Uri("../../Assets/Icons/SwitchOff.png", UriKind.Relative);
+        var onImage = new Uri("../../Assets/Icons/SwitchOn.png", UriKind.Relative);
+
+        var imagePath = IsAlarmEnabled ? onImage : offImage; 
+        
+        var icon = new BitmapImage(imagePath)
+        {
+            CacheOption = BitmapCacheOption.OnLoad
+        };
+        SwitchImage.Source = icon;
+    }
+
+    private void SwitchStatus(object sender, RoutedEventArgs e)
+    {
+        IsAlarmEnabled = !IsAlarmEnabled;
+        GetImageCorrespondingStatus();
+    }
+}
