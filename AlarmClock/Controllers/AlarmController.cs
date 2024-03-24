a020ecd24edd92e49697775413b12d74f7a2667e
xairaven <alexkovalevkpi@gmail.com>
2024-03-25 01:04:23 +0200

    (Logic) Feat: AlarmController logic template

--- AlarmClock/Controllers/AlarmController.cs
+++ AlarmClock/Controllers/AlarmController.cs
@@ -1,21 +1,48 @@
-﻿using System.Windows.Threading;
+﻿using AlarmClock.Repositories;
+using AlarmClock.Services;
 
 namespace AlarmClock.Controllers;
 
 public static class AlarmController
 {
-    public static void Start()
+    private static SettingsContext _context;
+    
+    public static void Start(SettingsContext context)
     {
-        var timer = new DispatcherTimer
-        {
-            Interval = TimeSpan.FromSeconds(30)
-        };
-        timer.Tick += CheckAlarms;
+        _context = context;
+        
+        var timer = new TimerService(CheckAlarms, TimeSpan.FromSeconds(30));
+
         timer.Start();
     }
 
     private static void CheckAlarms(object? sender, EventArgs e)
     {
-        throw new NotImplementedException();
+        foreach (var record in AlarmRepository.AlarmList)
+        {
+            var truncatedNow = DateTime.Now.Truncate(TimeSpan.FromMinutes(1));
+            var truncatedAlarmTime = record.DateTime.Truncate(TimeSpan.FromMinutes(1));
+
+            if (truncatedNow.CompareTo(truncatedAlarmTime) == 0)
+            {
+                // Show window of alarm 
+            }
+        }
+    }
+    
+    public static DateTime Truncate(this DateTime dateTime, TimeSpan timeSpan)
+    {
+        if (timeSpan == TimeSpan.Zero) return dateTime; // Or could throw an ArgumentException
+
+        // Some comments suggest removing the following line.  I think the check
+        // for MaxValue makes sense - it's often used to represent an indefinite expiry date.
+        // (The check for DateTime.MinValue has no effect, because DateTime.MinValue % timeSpan
+        // is equal to DateTime.MinValue for any non-zero value of timeSpan.  But I think
+        // leaving the check in place makes the intent clearer).
+        // YMMV and the fact that different people have different expectations is probably
+        // part of the reason such a method doesn't exist in the Framework.
+        if (dateTime == DateTime.MinValue || dateTime == DateTime.MaxValue) return dateTime; // do not modify "guard" values
+
+        return dateTime.AddTicks(-(dateTime.Ticks % timeSpan.Ticks));
     }
 }
